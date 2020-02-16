4598511a129128dbc769c60d68efb92a34dc2dd5
JuanRobles2164 <juanse2164@hotmail.com>
2020-02-16 18:16:44 -0500

    Controllers con CodeFirst
    Creación de controllers enlazados a las tablas del Code first.
    Y creación automática de los CRUD

---
+++ SaberPro/Controllers/DBController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SaberPro.Models;
+
+namespace SaberPro.Controllers
+{
+    public class DBController : Controller
+    {
+        private ClassContext db = new ClassContext();
+
+        // GET: DB
+        public ActionResult Index()
+        {
+            var estudiante = db.Estudiante.Include(e => e.id_Usuario_fk);
+            return View(estudiante.ToList());
+        }
+
+        // GET: DB/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiante estudiante = db.Estudiante.Find(id);
+            if (estudiante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiante);
+        }
+
+        // GET: DB/Create
+        public ActionResult Create()
+        {
+            ViewBag.Usuario_id = new SelectList(db.Usuario, "id", "Email");
+            return View();
+        }
+
+        // POST: DB/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Nombre,Apellidos,Estado,Nivel,id_Ciudad,Codigo_ProgAcademico,Usuario_id")] Estudiante estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Estudiante.Add(estudiante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Usuario_id = new SelectList(db.Usuario, "id", "Email", estudiante.Usuario_id);
+            return View(estudiante);
+        }
+
+        // GET: DB/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiante estudiante = db.Estudiante.Find(id);
+            if (estudiante == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Usuario_id = new SelectList(db.Usuario, "id", "Email", estudiante.Usuario_id);
+            return View(estudiante);
+        }
+
+        // POST: DB/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Nombre,Apellidos,Estado,Nivel,id_Ciudad,Codigo_ProgAcademico,Usuario_id")] Estudiante estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(estudiante).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Usuario_id = new SelectList(db.Usuario, "id", "Email", estudiante.Usuario_id);
+            return View(estudiante);
+        }
+
+        // GET: DB/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiante estudiante = db.Estudiante.Find(id);
+            if (estudiante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiante);
+        }
+
+        // POST: DB/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Estudiante estudiante = db.Estudiante.Find(id);
+            db.Estudiante.Remove(estudiante);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
